a76b0636670cb24eb5bb1af0d24874493730e0bc
Sunny Chan <schan6803@gmail.com>
2025-08-09 03:05:12 -0400

    adding the create to-do task endpoint requiring token authorization

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os 
 
 from fastapi import FastAPI, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel, EmailStr
 from typing import Annotated
 import firebase_admin
@@ -13,8 +13,12 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 import jwt
 from jwt.exceptions import InvalidTokenError
+import uuid
 
 load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 firebase_app = firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -34,9 +38,6 @@ class ToDo(BaseModel):
     title: str
     description: str
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
 # functions for password hashing and verifying 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -48,10 +49,15 @@ def verify_password(password, hashed_password):
     return pwd_context.verify(password, hashed_password)
 
 def get_user(email: EmailStr):
-    return db.collection("users").where(filter=FieldFilter("email", "==", email)).limit(1)
+    docs = list(db.collection("users").where(filter=FieldFilter("email", "==", email)).limit(1).stream())
+    if not docs:
+        return None
+    
+    return docs[0].to_dict()
+
 
 def authenticate_user(email: EmailStr, password: str):
-    user = get_user(email).to_dict()
+    user = get_user(email)
     if user is None:
         return False
     if not verify_password(password, user["password"]):
@@ -65,62 +71,60 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, os.getenv("SECRET_KEY"), algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credientials_exception = HTTPException(status_code=401, detail="Unauthorized") 
+    credientials_exception = HTTPException(status_code=401, detail="Unauthorized", headers={"WWW-Authenticate": "Bearer"}) 
     try:
-        payload = jwt.decode(token, os.getenv("SECRET_CODE"), algorithm=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get("email")
         if email is None:
             raise credientials_exception
     except InvalidTokenError:
         raise credientials_exception
     
-    query = db.collection("users").where(filter=FieldFilter("email", "==", email))
-    return {"email": email}
+    user = get_user(email)
+    if user is None:
+        raise credientials_exception
+    return user
 
 # User Registration Endpoint
 @app.post("/register")
 async def create_user(user: User):
-    query = get_user(user.email)
-    aggregate_query = aggregation.AggregationQuery(query)
-    aggregate_query.count(alias="all")
-    result = aggregate_query.get()[0][0].value
-
-    if result:
+    docs = db.collection("users").where(filter=FieldFilter("email", "==", user.email)).stream()
+    if any(docs):
         raise HTTPException(status_code=400, detail="A user has already been registered with this email address")
-        
     else:
-        user_dict = user.model_dump().copy()
+        user_dict = user.model_dump()
         user_dict["password"] = get_password_hash(user.password)
+        user_dict["user_id"] = str(uuid.uuid1())
         db.collection("users").add(user_dict)
         access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     
-    return {"token": create_access_token(user.model_dump, access_token_expires)}
+    return {"access_token": create_access_token({"email": user_dict["email"]}, access_token_expires), "token_type": "bearer"}
 
 
 # User Login
 @app.post("/login") 
-async def login(login_details : Login):
-    email = login_details.email
-    password = login_details.password
-
-    user = authenticate_user(email, password)
-
-    docs = db.collection("users").where(filter=FieldFilter("email", "==", email)).stream()
-    for doc in docs:
-        user = doc.to_dict()
-    
-    if verify_password(password, user["password"]):
-        return {"token": create_access_token({"email": email, "password": password}, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))}
+async def login(form_data : Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(form_data.username, form_data.password)
+    if user:
+        return {"access_token": create_access_token({"email": user["email"]}, timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)), "token_type": "bearer"}
     else:
-        raise HTTPException(status_code=400, detail="Password does not match")
+        raise HTTPException(status_code=400, detail="Invalid login details")
 
+
+@app.get("/me")
+async def debug(current_user: Annotated[dict, Depends(get_current_user)]):
+    return current_user
 
 @app.post("/todos")
-async def create_to_do(to_do: ToDo):
-
-    return {}
+async def create_to_do(to_do: ToDo, current_user : Annotated[dict, Depends(get_current_user)]):
+    to_do_dict = to_do.model_dump()
+    title = to_do_dict["title"]
+    desc = to_do_dict["description"]
+    to_do_dict["user_id"] = current_user["user_id"]
+    db.collection("tasks").add(to_do_dict)
+    return {"id": current_user["user_id"], "title": title, "description": desc}
 
